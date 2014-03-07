04e1be57ea9e200147c5d489ba6fbcf8a3b07000
Jane Kang <jkang@cs229-12.cs.pomona.edu>
2014-03-07 09:46:34 -0800

    Initial Commit

---
+++ ScrapbookWithCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrapbookWithCamera
+//
+//  Created by Jane Kang on 3/7/14.
+//  Copyright (c) 2014 Jane Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrapbookWithCameraTests/ScrapbookWithCameraTests.m
@@ -0,0 +1,34 @@
+//
+//  ScrapbookWithCameraTests.m
+//  ScrapbookWithCameraTests
+//
+//  Created by Jane Kang on 3/7/14.
+//  Copyright (c) 2014 Jane Kang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScrapbookWithCameraTests : XCTestCase
+
+@end
+
+@implementation ScrapbookWithCameraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
